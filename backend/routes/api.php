0add145e2aeb9db8fbc37ef8da932f0c295ca34e
Karlita43 <karla.vrsic@gmail.com>
2025-12-20 22:38:44 +0100

    ovo je ne gotova verzija backenda za favorites i user_ingredients

---
+++ backend/routes/api.php
@@ -0,0 +1,9 @@
+<?php
+
+use App\Http\Controllers\AuthController;
+
+Route::post('/login', [AuthController::class, 'login']);
+
+Route::middleware('auth:sanctum')->get('/me', function () {
+    return auth()->user();
+});
